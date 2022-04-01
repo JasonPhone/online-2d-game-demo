19e644056ac46a1a9e47c3a7449a0dbeca7e50f1
ja50n <zs_feng@qq.com>
2022-04-01 18:53:18 +0800

    use express

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,11 +13,25 @@
 
 // // Create and listen on 2567 (or PORT environment variable.)
 // listen(arenaConfig);
+import http from "http"
+import express from "express";
+import cors from "cors"
 import { Server } from "colyseus"
+import { monitor } from "@colyseus/monitor";
 import { DefaultRoom } from "./rooms/DefaultRoom";
-const port = 2567 
-const gameServer = new Server()
+import { WebSocketTransport } from "@colyseus/ws-transport";
+const port = 2567
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const gameServer = new Server({
+    transport: new WebSocketTransport({
+        server // 为 `WebSocketTransport` 提供自定义服务器
+    })
+});
 gameServer.define("DefaultRoom", DefaultRoom);
+app.use("/colyseus", monitor());
 gameServer.listen(port)
 console.log(`[GameServer] Listening on Port: ${port}`)
 
